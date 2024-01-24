0dfb894a445613d6909fbb6d36856bfb79b2a2db
abhishek <abhikoundal1019@gmail.com>
2024-01-24 16:45:26 +0530

    added two html pages singin and signup

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "blog/login.html")
 
+def signup(request):
+    return render(request, "blog/signup.html")
+
+def login(request):
+    return render(request, "blog/login.html")
+
